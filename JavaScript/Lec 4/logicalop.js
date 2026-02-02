9c3d80736642e9077850aaaa3dd10ec0261aa684
Shahid Jamal <shahidjamal5701@gmail.com>
2026-02-02 23:46:30 +0530

    lec 4
    added logical operator

---
+++ JavaScript/Lec 4/logicalop.js
@@ -0,0 +1,41 @@
+//logical operator
+//&& ,||
+
+//logical AND
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+console.log(false && true);
+
+//Logical OR
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
+console.log(false || true);
+
+//
+let a="Shahid";
+//let a = "";
+let b="Jamal"
+//let b="";
+let c=a&&b;
+console.log(c);
+
+let x=0;
+let y=10;
+console.log(x&&y);
+//&&:- if first value is false ,it will return the first value itself
+//If first value is true,it will return second
+
+//Logical OR ||
+
+//let r=10;
+let r=0;
+let s=56;
+console.log(r||s);
+//||:- if first value is true ,it will return the first value itself
+//If first value is false,it will return second
+
+//!= Not Equal to
+console.log(4!=5);
+console.log(5 != 5);

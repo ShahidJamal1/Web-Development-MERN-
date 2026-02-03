f804a21e9d9f12836a9e60d6648655535ee424fb
Shahid Jamal <shahidjamal5701@gmail.com>
2026-02-03 23:25:35 +0530

    lec 4
    conditional statement added

---
+++ JavaScript/Lec 4/conditionalstet.js
@@ -0,0 +1,17 @@
+//if-else condition
+let age1=19;
+if(age1>=18)
+    console.log("Eligble for Vote");
+else
+    console.log("Not Eligible for Vote")
+
+
+
+//else if condition
+let age=24;
+if(age>=18)
+    console.log("Young",age);
+else if(age>=35)
+    console.log("Old",age);
+else
+    console.log("Kids");

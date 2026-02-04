29b80be6be55f713dff3ba80d348a272fc5d4b28
Shahid Jamal <shahidjamal5701@gmail.com>
2026-02-04 23:44:37 +0530

    lec 4
    added looping

---
+++ JavaScript/Lec 4/looping.js
@@ -0,0 +1,22 @@
+//loop kaise kaam karta hai
+//for loop
+//post increment
+
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+//post decrement : i--
+
+//while loop
+let n=12;
+while(n<20){
+    console.log(n);
+    n++;
+}
+
+//Do while loop
+let m=5;
+do{
+    console.log(m);
+    m++;
+}while(m<9);

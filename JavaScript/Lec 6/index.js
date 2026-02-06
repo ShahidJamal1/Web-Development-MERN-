f6c2b70a2e478929793e46386a16d31097b0ab3c
Shahid Jamal <shahidjamal5701@gmail.com>
2026-02-06 23:31:53 +0530

    Lec 6
    Added

---
+++ JavaScript/Lec 6/index.js
@@ -0,0 +1,10 @@
+const str="Shahid Jamal";
+const str1='Welcome to'
+const str2=`Coder Army
+  Defence Series  `;//this is called template literals
+const day=21;
+const str4=`Meetup is Organised on ${day} February `//this is called string interpolation
+console.log(str);
+console.log(str1);
+console.log(str2);
+console.log(str4);
